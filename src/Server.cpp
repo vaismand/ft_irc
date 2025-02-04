813a7574dc44595e9040de955ca47640fe468b03
vaismand <omgemjey@gmail.com>
2025-02-04 12:12:58 +0100

    redesigned

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: dvaisman <dvaisman@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:38:10 by dvaisman          #+#    #+#             */
-/*   Updated: 2025/02/02 17:17:55 by dvaisman         ###   ########.fr       */
+/*   Updated: 2025/02/04 12:12:29 by dvaisman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,6 @@ void Server::run()
             perror("poll");
             break;
         }
-
         for (size_t i = 0; i < _pollfds.size(); i++)
 		{
             if (_pollfds[i].revents & POLLIN)
@@ -93,58 +92,55 @@ void Server::run()
     }
 }
 
-void Server::addClient() {
+void Server::addClient()
+{
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
     int client_fd = accept(_socket, (struct sockaddr*)&client_addr, &addr_len);
-    if (client_fd < 0) {
+    if (client_fd < 0)
+    {
         perror("accept");
         return;
     }
 
-    // Set client socket to non-blocking
     fcntl(client_fd, F_SETFL, O_NONBLOCK);
 
-    // Add new client to poll set
     struct pollfd client_pollfd;
     client_pollfd.fd = client_fd;
     client_pollfd.events = POLLIN;
     _pollfds.push_back(client_pollfd);
 
-    // Store client in the map
     _clients[client_fd] = Client(client_fd);
 
     std::cout << "New client connected: " << client_fd << std::endl;
 }
 
-void Server::handleClient(int fd) {
+void Server::handleClient(int fd)
+{
     char buffer[1024];
     memset(buffer, 0, sizeof(buffer));
 
     ssize_t bytes_received = recv(fd, buffer, sizeof(buffer) - 1, 0);
-    if (bytes_received <= 0) {
+    if (bytes_received <= 0)
+    {
         removeClient(fd);
         return;
     }
-
     buffer[bytes_received] = '\0';
     std::cout << "Received from " << fd << ": " << buffer << std::endl;
-
-    // Here you can process client messages based on IRC commands
 }
 
-void Server::removeClient(int fd) {
+void Server::removeClient(int fd)
+{
     std::cout << "Client disconnected: " << fd << std::endl;
     close(fd);
 
-    // Remove from poll set
-    for (size_t i = 0; i < _pollfds.size(); i++) {
+    for (size_t i = 0; i < _pollfds.size(); i++)
+    {
         if (_pollfds[i].fd == fd) {
             _pollfds.erase(_pollfds.begin() + i);
             break;
         }
     }
-
-    // Remove from client map
     _clients.erase(fd);
 }
