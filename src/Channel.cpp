250831fdb2c5581460d485e89138d035a1caa34a
Ruben Pin <pinchasov.ruben@gmail.com>
2025-02-12 15:07:09 +0100

    fd_changes_Channnel

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -5,11 +5,11 @@ Channel::Channel(const std::string& name, const std::string& pass) : _cName(name
 Channel::~Channel() {}
 
 // ----- getter Functions -----
-std::string Channel::getcName() const { return _cName; }
-std::string Channel::getcTopic() const { return _cTopic; }
-std::string Channel::getcPass() const { return _cPass; }
-bool Channel::getChannelType() const { return _isInviteOnly; }
-const std::vector<Client*>& Channel::getJoined() const { return _joined; }
+const std::string& Channel::getcName() const { return _cName; }
+const std::string& Channel::getcTopic() const { return _cTopic; }
+const std::string& Channel::getcPass() const { return _cPass; }
+const bool& Channel::getChannelType() const { return _isInviteOnly; }
+const std::vector<int>& Channel::getJoined() const { return _joined; }
 
 // ----- setter Functions -----
 void Channel::setcName(const std::string& name) { _cName = name; }
@@ -17,40 +17,33 @@ void Channel::setcTopic(const std::string& topic) { _cTopic = topic; }
 void Channel::setcPass(const std::string& password) { _cPass = password; }
 void Channel::setChannelType() { _isInviteOnly = !_isInviteOnly; }
 
-void Channel::addClient(Client *client) 
+void Channel::addClient(int fd) 
 { 
 	for(size_t i = 0; i < _joined.size(); i++)
 	{
-		if (_joined[i] == client)
+		if (_joined[i] == fd)
 			return;
 	}
-	_joined.push_back(client);
+	_joined.push_back(fd);
 }
 
-void Channel::rmClient(int fd) {
-    for (std::vector<Client*>::iterator it = _joined.begin(); it != _joined.end(); )
+void Channel::rmClient(int fd)
+{
+    for (std::vector<int>::iterator it = _joined.begin(); it != _joined.end(); )
     {
-        if ((*it)->getFd() == fd)
-        {
+        if (*it == fd)
             it = _joined.erase(it);
-        } 
         else
-        {
             ++it;
-        }
     }
 }
 
 void Channel::rmOperator(int fd) {
-    for (std::vector<Client*>::iterator it = _operators.begin(); it != _operators.end(); )
+    for (std::vector<int>::iterator it = _operators.begin(); it != _operators.end(); )
     {
-        if ((*it)->getFd() == fd)
-        {
+        if (*it == fd)
             it = _operators.erase(it);
-        } 
         else
-        {
             ++it;
-        }
     }
 }
